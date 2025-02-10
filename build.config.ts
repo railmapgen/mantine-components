0d0b89ecb87cc94a98c859eb1e82f303e9d5f010
Chito Wong <chi.to.wong@outlook.com>
2025-02-11 00:07:29 +0800

    Fix JSX transform

--- build.config.ts
+++ build.config.ts
@@ -1,1 +1,10 @@
-export default {};
+import { defineBuildConfig } from 'unbuild';
+
+export default defineBuildConfig({
+    rollup: {
+        esbuild: {
+            // This line is the important one
+            jsx: 'automatic',
+        },
+    },
+});
