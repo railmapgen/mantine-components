2f55947797c97d6d372bb9a28ccc924d5fcf7932
Chito Wong <chi.to.wong@outlook.com>
2025-02-11 14:47:06 +0800

    Try to fix postcss bundling

--- build.config.ts
+++ build.config.ts
@@ -4,10 +4,18 @@ import postcss from 'rollup-plugin-postcss';
 export default defineBuildConfig({
     hooks: {
         'rollup:options'(_ctx, options) {
-            options.plugins.unshift(postcss({ extract: true }));
+            options.plugins.pop(); // remove rawPlugin
+            options.plugins.push(postcss({ extract: true, modules: true }));
         },
     },
     rollup: {
+        output: {
+            // preserveModules: true,
+        },
+        resolve: { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
+        replace: {
+            preventAssignment: true,
+        },
         esbuild: {
             jsx: 'automatic',
         },
