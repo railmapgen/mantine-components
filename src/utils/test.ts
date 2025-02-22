a857325204e1a59d11cb2e66cfd6c5d25ecafea1
Chito Wong <chi.to.wong@outlook.com>
2025-02-22 16:14:38 +0800

    Make typescript happy

--- src/utils/test.ts
+++ src/utils/test.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 import ResizeObserverPolyfill from 'resize-observer-polyfill';
 import { TextEncoder } from 'node:util';
 
